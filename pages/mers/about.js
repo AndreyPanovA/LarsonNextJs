590ad37be17c3b2f41a0b941cc6d4ad1390f3b12
AndreyPanovA <a.andrey.panov@gmail.com>
2020-09-17 01:45:58 +0300

    mers

---
+++ pages/mers/about.js
@@ -0,0 +1,25 @@
+import { MainLayout } from "../../components/MainLayout";
+import Navigation from "../../components/navigation/Navigation";
+import dataStorage from "../../components/dataStorage/dataStorage";
+import TextBlack from "../../components/textBlack/TextBlack";
+import { connect } from "react-redux";
+function About({ lang }) {
+  const { about: url } = dataStorage.backgroundsUrl.mers;
+  let { about } = dataStorage.mers.pages;
+  return (
+    <MainLayout url={url}>
+      <Navigation site="servis-volvo" />
+      <div style={{ position: "relative", fontSize: 55 }}>
+        {about[0].h1[lang]}
+      </div>
+      <div style={{ position: "relative", fontSize: 55 }}>
+        {about[1].h1[lang]}
+      </div>
+      <TextBlack isImg={true}>
+        {about.map((el, idx) => (el.p ? <p key={idx}>{el.p[lang]}</p> : null))}
+      </TextBlack>
+    </MainLayout>
+  );
+}
+
+export default connect(({ lang }) => ({ lang }))(About);
