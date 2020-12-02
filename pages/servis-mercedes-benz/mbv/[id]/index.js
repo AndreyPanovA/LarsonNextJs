59f4ca8e7c70794c2830ea746a8d580aa0634f5c
AndreyPanovA <a.andrey.panov@gmail.com>
2020-12-02 17:45:38 +0300

    t

--- pages/servis-mercedes-benz/mbv/[id]/index.js
+++ pages/servis-mercedes-benz/mbv/[id]/index.js
@@ -13,14 +13,14 @@ const { cn } = LogicServ;
 
 const MbMap = () => {
   return (
-    <div className={cls["serv-map"]}>
+    <div className={cls["serv-map"]} style={{paddingBottom:55}}>
       <div className={cls["serv-map-line"]}></div>
       <div className={cls["serv-container"]}>
         <div className={cls["serv-map-contacts"]}>
           <div className={cls["contacts-container"]}>
             <div className={cls["map-flex"]}>
               <h2>Контакты</h2>
-              <img src="../assets/img/larson-white.svg" alt="Larson" />
+              <img src="/assets/img/larson-white.svg" alt="Larson" />
             </div>
             <p>Ежедневно в 9:00 до 20:00</p>
             <p>Единый телефон: +7 (495) 781-10-81.</p>
@@ -45,6 +45,7 @@ const MbMap = () => {
           </div>
         </div>
         <iframe
+          style={{marginTop:15, width:"300px", height:"300px"}}
           src="https://yandex.ru/map-widget/v1/?um=constructor%3A6e59348621f8f7d443d881e0e9454b2d4df2ca1b9b90391de232e09144a4129f&amp;source=constructor"
           width="100%"
           height="720"
@@ -134,8 +135,15 @@ const MbItems =(props)=>{
         
         
         }
-        <div className={cn(cls['container-map'])}><Tour text="Виртуальный тур" />
-      <MbMap />
+        <div className={cn(cls['container-map'])}>
+          <Tour text="Виртуальный тур" />
+        <iframe
+          style={{marginTop:15, width:"100%", height:"300px"}}
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3A6e59348621f8f7d443d881e0e9454b2d4df2ca1b9b90391de232e09144a4129f&amp;source=constructor"
+          width="100%"
+          height="720"
+          frameBorder="0"
+        ></iframe>
       </div>
     </MainLayout>
     </>
